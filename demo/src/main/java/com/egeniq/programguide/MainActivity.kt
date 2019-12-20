97c6ed1692f3d924296b29e2d91c687e483c60f2
Dániel Zolnai <zoiddani@gmail.com>
2019-12-20 09:03:48 +0100

    Fix crash on missing 310 backport init

--- demo/src/main/java/com/egeniq/programguide/MainActivity.kt
+++ demo/src/main/java/com/egeniq/programguide/MainActivity.kt
@@ -16,6 +16,7 @@ package com.egeniq.programguide
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import com.jakewharton.threetenabp.AndroidThreeTen
 
 /**
  * Loads [EpgFragment].
@@ -24,6 +25,8 @@ class MainActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Initialize the 310 backport lib. Usually you do this in your application object
+        AndroidThreeTen.init(this)
         setContentView(R.layout.activity_main)
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, EpgFragment())
